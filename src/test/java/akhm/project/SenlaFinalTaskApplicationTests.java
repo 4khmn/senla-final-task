6319cb7dab9bc11c8add8d7334fdf0ef9386b0ae
AkhminDenis <denis-akhmin@yandex.ru>
2026-04-03 16:29:36 +0300

    init commit

---
+++ src/test/java/akhm/project/SenlaFinalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package akhm.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenlaFinalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
