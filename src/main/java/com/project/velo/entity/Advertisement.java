1f4941f443a6853c32ceba890b954de5176a8da3
AkhminDenis <denis-akhmin@yandex.ru>
2026-04-03 18:11:49 +0300

    добавление сущности объявления

---
+++ src/main/java/com/project/velo/entity/Advertisement.java
@@ -0,0 +1,62 @@
+package com.project.velo.entity;
+
+import com.project.velo.entity.enums.AdStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "advertisements")
+public class Advertisement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank(message = "Название объявления не может быть пустым")
+    @Size(max = 255, message = "Название объявления должно быть до 255 символов")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "Описание объявления не может быть пустым")
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @NotNull(message = "Цена должна быть указана")
+    @PositiveOrZero(message = "Слишком большая цена или много знаков после запятой")
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id", nullable = false)
+    private User seller;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "is_top")
+    private boolean isTop;
+
+    @Enumerated(EnumType.STRING)
+    private AdStatus status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
