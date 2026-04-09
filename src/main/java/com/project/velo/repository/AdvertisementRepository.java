903fc9c7aacb1826cebef7ec754ece974571e3af
AkhminDenis <denis-akhmin@yandex.ru>
2026-04-09 21:10:54 +0300

    Добавление сервиса с @Scheduled для очистки бд от грязных данных

--- src/main/java/com/project/velo/repository/AdvertisementRepository.java
+++ src/main/java/com/project/velo/repository/AdvertisementRepository.java
@@ -3,6 +3,7 @@ package com.project.velo.repository;
 import com.project.velo.entity.Advertisement;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -15,9 +16,19 @@ public class AdvertisementRepository extends BaseRepository<Advertisement, Long>
 
     public List<Advertisement> findAllByUsername(String username) {
         return entityManager.createQuery(
-                "SELECT a FROM Advertisement a WHERE seller.username = :username AND a.status = 'ACTIVE' ORDER BY a.createdAt ASC", Advertisement.class)
+                "SELECT a FROM Advertisement a WHERE seller.username = :username " +
+                        "AND a.status = 'ACTIVE' ORDER BY a.createdAt ASC", Advertisement.class)
                 .setParameter("username", username)
                 .getResultList();
+    }
+
+
+    public int resetExpiredTopFlags(LocalDateTime now) {
+        return entityManager.createQuery(
+                        "UPDATE Advertisement a SET a.top = false " +
+                                "WHERE a.top = true AND a.topUntil < :now")
+                .setParameter("now", now)
+                .executeUpdate();
     }
 
 
---
+++ src/main/java/com/project/velo/service/AdvertisementCleanUpService.java
@@ -0,0 +1,32 @@
+package com.project.velo.service;
+
+import com.project.velo.repository.AdvertisementRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AdvertisementCleanUpService {
+
+    private final AdvertisementRepository advertisementRepository;
+
+    // at 03:00
+    @Scheduled(cron = "0 0 3 * * *")
+    @Transactional
+    public void cleanupExpiredTops() {
+        log.info("Starting scheduled cleanup of expired TOP statuses...");
+
+        int updatedCount = advertisementRepository.resetExpiredTopFlags(LocalDateTime.now());
+
+        log.info("Cleanup finished. Reset {} advertisements.", updatedCount);
+    }
+
+
+
+}
