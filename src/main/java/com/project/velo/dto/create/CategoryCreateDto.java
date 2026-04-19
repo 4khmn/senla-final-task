4a476b63376b3b29b77df7221421f29125d3f944
AkhminDenis <denis-akhmin@yandex.ru>
2026-04-19 14:54:06 +0300

    добавление класса ImageControllerTest

--- src/main/java/com/project/velo/dto/create/CategoryCreateDto.java
+++ src/main/java/com/project/velo/dto/create/CategoryCreateDto.java
@@ -1,10 +1,11 @@
 package com.project.velo.dto.create;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CategoryCreateDto(
-        @NotBlank
-        @Max(value = 48, message = "Название категории не может быть больше 48 символов")
-        String name) {
-}
+        @NotBlank(message = "Название не может быть пустым")
+        @Size(min = 2, message = "Название категории не может быть меньше 2 символов")
+        @Size(max = 48, message = "Название категории не может быть больше 48 символов")
+        String name
+) {}
--- src/main/java/com/project/velo/dto/update/CategoryUpdateDto.java
+++ src/main/java/com/project/velo/dto/update/CategoryUpdateDto.java
@@ -1,10 +1,11 @@
 package com.project.velo.dto.update;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CategoryUpdateDto(
-        @NotBlank
-        @Max(value = 48, message = "Название категории не может быть больше 48 символов")
+        @NotBlank(message = "Название не может быть пустым")
+        @Size(min = 2, message = "Название категории не может быть меньше 2 символов")
+        @Size(max = 48, message = "Название категории не может быть больше 48 символов")
         String name) {
 }
