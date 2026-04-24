7a908cd679cfc285bd0e1f6845fa7d68771c0cb7
AkhminDenis <denis-akhmin@yandex.ru>
2026-04-24 18:09:17 +0300

    документация для дествий вокруг пользователя + изображений

--- src/main/java/com/project/velo/controller/infrastructure/ImageController.java
+++ src/main/java/com/project/velo/controller/infrastructure/ImageController.java
@@ -2,6 +2,9 @@ package com.project.velo.controller.infrastructure;
 
 import com.project.velo.dto.infrastracture.MediaResource;
 import com.project.velo.service.storage.FileStorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Images", description = "Просмотр изображений")
 @RestController
 @RequestMapping("/api/images")
 @RequiredArgsConstructor
@@ -20,6 +24,9 @@ public class ImageController {
 
     private final FileStorageService storageService;
 
+    @Operation(summary = "Получение изображения")
+    @ApiResponse(responseCode = "404", description = "Изображение не найдено")
+    @ApiResponse(responseCode = "200")
     @GetMapping("/{folder}/{filename}")
     public ResponseEntity<Resource> getImage(@PathVariable String folder, @PathVariable String filename) {
         log.info("GET /api/images/{}/{} - Fetching image by folder: {} and by filename: {}", folder, filename, folder, filename);
