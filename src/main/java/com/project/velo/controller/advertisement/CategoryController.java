81fe8f39f0dc53bc36ce89493534fc26ddf23b9e
AkhminDenis <denis-akhmin@yandex.ru>
2026-04-24 14:06:04 +0300

    добавление документации для контроллера CategoryController

--- src/main/java/com/project/velo/controller/advertisement/CategoryController.java
+++ src/main/java/com/project/velo/controller/advertisement/CategoryController.java
@@ -2,6 +2,8 @@ package com.project.velo.controller.advertisement;
 
 import com.project.velo.dto.response.advertisement.CategoryResponseDto;
 import com.project.velo.service.advertisement.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Categories", description = "Управление категориями")
 @RestController
 @Slf4j
 @RequestMapping("/api/categories")
@@ -17,8 +20,8 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-
-
+    
+    @Operation(summary = "Получить список доступных категорий")
     @GetMapping
     public ResponseEntity<List<CategoryResponseDto>> getAllCategories() {
         log.info("GET /api/categories - fetching all categories");
