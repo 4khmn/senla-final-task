2444b35db67070260fe88c6beca716c76c44db4a
AkhminDenis <denis-akhmin@yandex.ru>
2026-06-04 18:02:13 +0300

    добавление миграции V9

---
+++ src/main/resources/db/migration/V9__add_favorites.sql
@@ -0,0 +1,11 @@
+CREATE TABLE favorites (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    advertisement_id BIGINT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (user_id, ad_id),
+
+    CONSTRAINT fk_favorites_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_favorites_ad FOREIGN KEY (advertisement_id) REFERENCES advertisements(id) ON DELETE CASCADE
+)
